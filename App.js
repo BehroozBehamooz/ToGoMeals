29323fe4194e244a1aa53f28cc76eb390f9e016e
Behrooz Behamooz <behrooz.b.ir@gmail.com>
2022-09-02 16:39:29 -0700

    added theme and theme provider

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { ThemeProvider } from "styled-components/native";
 import { RestuarantsScreen } from "./src/features/restuarants/screens/restuarants.screen";
+
+import { theme } from "./src/infrastructure/theme";
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <RestuarantsScreen />
-    </>
+    </ThemeProvider>
   );
 }
