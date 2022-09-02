5baca3d559b408d548c52c50b08ba1a0427726d6
Behrooz Behamooz <behrooz.b.ir@gmail.com>
2022-09-01 21:41:34 -0700

    added and configured eslint and prettier

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 
 export default function App() {
   return (
     <>
       <SafeAreaView style={styles.container}>
-          <View style={{ padding: 16, backgroundColor: "green"}} >
-            <Text>search</Text>
-          </View>
-          <View style={{ flex: 1, padding: 16, backgroundColor: "blue"}} >
-            <Text>list</Text>
-          </View>
+        <View style={styles.search}>
+          <Text>search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>list</Text>
+        </View>
       </SafeAreaView>
     </>
   );
@@ -20,4 +21,6 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: StatusBar.currentHeight,
   },
+  search: { padding: 16, backgroundColor: "green" },
+  list: { flex: 1, padding: 16, backgroundColor: "blue" },
 });
