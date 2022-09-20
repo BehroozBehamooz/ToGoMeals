486f1477f1071e85f14e9ce2f3e8332900b1deb5
Behrooz Behamooz <behrooz.b.ir@gmail.com>
2022-09-19 21:45:01 -0700

    added List.Accordion for the menue

--- src/features/restuarants/screens/restaurant-detail.screen.js
+++ src/features/restuarants/screens/restaurant-detail.screen.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { ScrollView } from "react-native";
+import { List } from "react-native-paper";
 
 import { RestuarantInfoCard } from "../components/restuarant-info-card.component";
 
@@ -9,6 +11,41 @@ export const RestaurantDetailScreen = ({ route }) => {
   return (
     <SafeAreaView>
       <RestuarantInfoCard restaurant={restaurant} />
+      <ScrollView>
+        <List.Section title="Menu">
+          <List.Accordion
+            title="Breakfast"
+            left={(props) => <List.Icon {...props} icon="food-croissant" />}
+          >
+            <List.Item title="Breakfasr Burito" />
+            <List.Item title="Eggs Benedic" />
+          </List.Accordion>
+
+          <List.Accordion
+            title="Lunch"
+            left={(props) => <List.Icon {...props} icon="food-fork-drink" />}
+          >
+            <List.Item title="Fried Chicken Sandwitch" />
+            <List.Item title="Salmon Wrap" />
+          </List.Accordion>
+
+          <List.Accordion
+            title="Dinner"
+            left={(props) => <List.Icon {...props} icon="food-variant" />}
+          >
+            <List.Item title="Italian Meatballs" />
+            <List.Item title="Crispy Calamary" />
+          </List.Accordion>
+
+          <List.Accordion
+            title="Drinks"
+            left={(props) => <List.Icon {...props} icon="bottle-wine" />}
+          >
+            <List.Item title="Moscow mule" />
+            <List.Item title="Mai Tai" />
+          </List.Accordion>
+        </List.Section>
+      </ScrollView>
     </SafeAreaView>
   );
 };
